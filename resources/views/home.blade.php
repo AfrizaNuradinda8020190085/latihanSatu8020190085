8c1fd78a0a973c72317a1acdf7d2cd34e7e042f6
AfrizaNuradinda8020190085 <afrizanuradinda8@gmail.com>
2021-11-11 13:59:43 +0700

    commit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+
+                    <h2>{{ $title }}</h2>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pengguna as $baris)
+                                <tr>
+                                    <td>{{ $baris->name }}</td>
+                                    <td>{{ $baris->email }}</td>
+                                </tr>
+                            @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
